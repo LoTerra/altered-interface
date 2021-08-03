a6ec8442cfdb57a30866ef279c416dfe5ed09793
Timboow <tim@yourcreativebrand.nl>
2021-08-03 18:31:15 +0200

    editable eq

--- src/components/Equilibrium.jsx
+++ src/components/Equilibrium.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Equilibrium(props) {
 
+    const colorGood_0 = {start: '#909D0D', stop:'#E9FF03'}
+    const colorGood_1 = {start: '#D0E026', stop:'#7E8906'}
+    const colorGood_2 = {start: '#D0E026', stop:'#E9FF00'}
+
+    const colorBad_0 = {start: '#9D0D0D', stop:'#FF0303'}
+    const colorBad_1 = {start: '#E02626', stop:'#890606'}
+    const colorBad_2 = {start: '#E02626', stop:'#FF0000'}
+
    return (
     <svg width="396" height="138" viewBox="0 0 396 138" fill="none"  xmlns="http://www.w3.org/2000/svg">
     <path d="M-2.96495e-06 70.1697L51.2718 0.000287914L100.572 70.1697L51.2718 138L-2.96495e-06 70.1697Z" fill="url(#paint0_linear)" fill-opacity="0.81"/>
@@ -12,16 +20,16 @@ export default function Equilibrium(props) {
     <path d="M198 70.3018L297.442 70.3018L345.715 3.05176e-05L198 70.3018Z" fill="url(#paint5_linear)" fill-opacity="0.89"/>
     <defs>
     <linearGradient id="paint0_linear" x1="40.5484" y1="138.204" x2="68.9873" y2="38.9465" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#909D0D"/>
-    <stop offset="1" stop-color="#E9FF03"/>
+    <stop stop-color={colorGood_0.start}/>
+    <stop offset="1" stop-color={colorGood_0.stop}/>
     </linearGradient>
     <linearGradient id="paint1_linear" x1="50.2854" y1="104.151" x2="198" y2="104.151" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#D0E026"/>
-    <stop offset="1" stop-color="#7E8906"/>
+    <stop stop-color={colorGood_1.start}/>
+    <stop offset="1" stop-color={colorGood_1.stop}/>
     </linearGradient>
     <linearGradient id="paint2_linear" x1="50.2854" y1="35.1509" x2="198" y2="35.1509" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#D0E026"/>
-    <stop offset="1" stop-color="#E9FF00"/>
+    <stop stop-color={colorGood_2.start}/>
+    <stop offset="1" stop-color={colorGood_2.stop}/>
     </linearGradient>
     <linearGradient id="paint3_linear" x1="355.452" y1="138.204" x2="327.013" y2="38.9465" gradientUnits="userSpaceOnUse">
     <stop stop-color="#1E1E1E"/>
